a141d299fa9b2f4dc8d177a837275ba9170f4edd
tatiana.mojica@bustle.com <tatiana.mojica@bustle.com>
2018-08-18 19:39:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,73 @@
+var number=5
+
+number++
+
+number
+
+number--
+
+number 
+
+1 + 80 
+
+60 - 40
+
+2 * 3.4
+
+5.0 / 2.5
+
+function add(a,b) {
+ return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+var number = 10
+
+function add5 () {
+  number += 5 
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
+
+function inc(n) {
+  return (n + 1)
+}
+  
+function dec(n) {
+  return (n - 1)
+}
+
+function makeInt(n) {
+  return parseInt('n',)
+}
